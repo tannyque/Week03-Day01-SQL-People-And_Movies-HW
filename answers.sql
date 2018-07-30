19c7b1781278a30ef40a952be4c2daa697cd91d6
tannyque <tannyque1992@gmail.com>
2018-07-30 17:57:02 +0100

    Answers all questions accurately

--- answers.sql
+++ answers.sql
@@ -1,18 +1,26 @@
 -- 1. Return ALL the data in the 'movies' table.
 SELECT * FROM movies;
+
 -- 2. Return ONLY the name column from the 'people' table
-SELECT * FROM people;
+SELECT name FROM people;
+
 -- 3. Oops! Someone at CodeClan spelled Andrew's surname wrong! Change it to reflect the proper spelling ('Andrew Craggie' should be 'Andrew Craigie').
 UPDATE people SET name = 'Andrew Craigie' WHERE name = 'Andrew Craggie';
+
 -- 4. Return ONLY your name from the 'people' table.
-SELECT * FROM people WHERE name = 'Tanwir Qureshi';
+SELECT * FROM people WHERE id = 19;
+
 -- 5. The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
 DELETE FROM movies WHERE title = 'Batman Begins';
+
 -- 6. Create a new entry in the 'people' table with the name of one of the instructors.
 INSERT INTO people (name) VALUES ('Ally McGilloway');
+
 -- 7. Ben Affleck has decided to hijack our movie evening. Typical DC behaviour! Remove him from the table of people.
 DELETE FROM people WHERE name = 'Ben Affleck';
+
 -- 8. The cinema has just heard that they will be holding an exclusive midnight showing of 'Captain Marvel'!! Create a new entry in the 'movies' table to reflect this.
-INSERT INTO movies (title) VALUES ('Captain Marvel');
+INSERT INTO movies (title, year, show_time) VALUES ('Captain Marvel', 2019, '00:00');
+
 -- 9. The cinema would also like to make the Guardians movies a back to back feature. Find out the show time of "Guardians of the Galaxy" and set the show time of "Guardians of the Galaxy 2" to start two hours later.
-UPDATE movies SET show_time = '23:20' WHERE title = 'Guardians of the Galaxy 2';
+UPDATE movies SET show_time = '23:20' WHERE id = 16;
